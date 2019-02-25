54b2f91e0087893a52f16b4d0202eed3213dfb79
Icarus Websolutions <47674100+IcarusWS@users.noreply.github.com>
2019-02-25 09:43:57 +0100

    Saves the components in dlos(typeof object)

--- js/src/daidalos.js
+++ js/src/daidalos.js
@@ -1,8 +1,20 @@
 // ES5 Daidalos Library
 var d = function( component )
 {
-    var element = document.getElementById(component);
-    if(element){ return new Component(element) }
-    else { return false };
+    
+    if(dlos[component])
+    {
+        return dlos[component];
+    }
+    else {
+        var element = document.getElementById(component);
+        if(element){ 
+            var cmp = new Component(element);
+            dlos[component] = cmp;
+            return cmp;
+        }
+        else { return false };
+    }
 }
 
+var dlos = {};
