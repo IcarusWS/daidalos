f24952511918aa95b235afceea1b8e6d4b90e441
Jesse <44534584+PXJesse@users.noreply.github.com>
2019-03-02 22:48:10 +0100

    Reprogrammed as IIFE

--- js/src/css_anims.js
+++ js/src/css_anims.js
@@ -1,13 +1,13 @@
-// Config params
-const navbarPills = document.querySelector('.cl-navbar-pills');
-
-
-if (navbarPills != null) {
-  window.onscroll = () => {
-    if(this.scrollY <= 10) {
-      if (navbarPills.classList.contains('scroll')) {navbarPills.classList.remove('scroll'); } else {}
-    } else {
-      navbarPills.classList.add('scroll');
-    }
-  };
-}
+(function() {
+  const navbarPills = document.querySelector('.cl-navbar-pills');
+  
+  if (navbarPills != null) {
+    window.onscroll = () => {
+      if(this.scrollY <= 10) {
+        if (navbarPills.classList.contains('scroll')) {navbarPills.classList.remove('scroll'); } else {}
+      } else {
+        navbarPills.classList.add('scroll');
+      }
+    };
+  }
+})();
